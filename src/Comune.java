ea67c7195d8f1c4521439dc052a15a715fb5d4b2
E343C <e.cesari@studenti.unibs.it>
2021-04-26 18:42:21 +0200

    1.4

---
+++ src/Comune.java
@@ -0,0 +1,17 @@
+public class Comune {
+        private String nome;
+        private String codice;
+
+        public Comune(String nome, String codice) {
+                this.nome=nome;
+                this.codice=codice;
+        }
+
+        public String getCodice() {
+                return codice;
+        }
+
+        public String getNome() {
+                return nome;
+        }
+}
---
+++ src/Data.java
@@ -0,0 +1,36 @@
+public class Data {
+    private int anno;
+    private int mese;
+    private int giorno;
+
+    public Data(int anno, int mese, int giorno) {
+        this.anno = anno;
+        this.mese = mese;
+        this.giorno = giorno;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public int getMese() {
+        return mese;
+    }
+
+    public void setMese(int mese) {
+        this.mese = mese;
+    }
+
+    public int getGiorno() {
+        return giorno;
+    }
+
+    public void setGiorno(int giorno) {
+        this.giorno = giorno;
+    }
+
+}
