9b3a4f72913c402b23f2365248035ceee685665c
E343C <e.cesari@studenti.unibs.it>
2021-04-29 17:10:10 +0200

    2.1

--- src/Comune.java
+++ src/Comune.java
@@ -1,6 +1,6 @@
-public class Comune {
-        private String nome;
-        private String codice;
+public class Comune {           // credo sia abbastanza self-explaining
+	final String nome;
+	final String codice;
 
 	public Comune(String nome, String codice) {
 		this.nome = nome;
--- src/Reader.java
+++ src/Reader.java
@@ -4,90 +4,95 @@ import javax.xml.stream.XMLStreamConstants;
 import java.io.FileInputStream;
 
 public class Reader {
-   /* private static final String FILE_NAME_CODICI = "codiciFiscali.xml";
-    private static final String FILE_NAME_COMUNI = "comuni.xml";
-    private static final String FILE_NAME_PERSONE = "inputPersone.xml";*/
+	private static String filenameCod = "codiciFiscali.xml";
+	private static String filenamePers = "inputPersone.xml";
 
-    public static void readCodiciFiscali() {
-        XMLInputFactory xmlif = null;
-        XMLStreamReader xmlr = null;
+	/**
+	 * legge il file dei codici fiscali e aggiunge il codice alla lista codici
+	 */
+	public static void readCodiciFiscali() {
+		XMLInputFactory xmlif = null;
+		XMLStreamReader xmlr = null;
 
-        try {
-            int size = 0;
-            xmlif = XMLInputFactory.newInstance();
-            xmlr = xmlif.createXMLStreamReader(new FileInputStream("XML/input/codiciFiscali.xml"));
-            while (xmlr.hasNext()) { // continua a leggere finché ha eventi a disposizione
-                if (xmlr.getEventType() == XMLStreamConstants.CHARACTERS)
-                {
-                    String s = xmlr.getText();
-                    if(s.contains("\n")==false) //Questo if serve ad evitare che le varie stringhe vengano sostituite da "\n" e sue varianti tipo "\n "
-                        DataProcessing.addToCodici(s);
-                }
-                xmlr.next();
-            }
-        } catch (Exception e) {
-            System.out.println("Errore in readCodiciFiscali");
-            System.out.println(e.getMessage());
-        }
-    }
+		try {
+			xmlif = XMLInputFactory.newInstance();
+			xmlr = xmlif.createXMLStreamReader(new FileInputStream("XML/input/" + filenameCod));
+			while (xmlr.hasNext()) { // continua a leggere finché ha eventi a disposizione
+				if (xmlr.getEventType() == XMLStreamConstants.CHARACTERS) {
+					String s = xmlr.getText();
+					if (!s.contains("\n")) //Questo if serve ad evitare che le varie stringhe vengano sostituite da "\n" e sue varianti tipo "\n  "
+						DataProcessing.addToCodici(s);
+				}
+				xmlr.next();
+			}
+		} catch (Exception e) {
+			System.out.println("Errore in readCodiciFiscali");
+			System.out.println(e.getMessage());
+		}
+	}
 
-    public static void readInputPersone() {
-        XMLInputFactory xmlif = null;
-        XMLStreamReader xmlr = null;
+	/**
+	 * legge il file delle persone, crea un'istanza di persona e la adda alla lista che le contiene
+	 */
+	public static void readInputPersone() {
+		XMLInputFactory xmlif = null;
+		XMLStreamReader xmlr = null;
 
-        try {
-            int id = 0;
-            String tagName = "";
-            String nome="", cognome="", comune="", sesso="";
-            Data dataNascita = null;
-            xmlif = XMLInputFactory.newInstance();
-            xmlr = xmlif.createXMLStreamReader(new FileInputStream("XML/input/inputPersone.xml"));
-            while (xmlr.hasNext()) { // continua a leggere finché ha eventi a disposizione
-                switch (xmlr.getEventType()) { // switch sul tipo di evento
-                    case XMLStreamConstants.START_ELEMENT: // inizio di un elemento: stampa il nome del tag e i suoi attributi
-                        tagName = xmlr.getLocalName();
-                        if(tagName.equals("persona"))
-                            id = Integer.parseInt(xmlr.getAttributeValue(0));
-                        break;
-                    case XMLStreamConstants.END_ELEMENT: // fine di un elemento: stampa il nome del tag chiuso
-                       if(xmlr.getLocalName().equals("persona"))
-                           DataProcessing.addToPersone(new Persona(nome, cognome, comune, sesso, dataNascita, id));
-                        break;
-                    case XMLStreamConstants.CHARACTERS: // content all’interno di un elemento: stampa il testo
-                        String s = xmlr.getText();
-                        if(s.contains("\n")) //Questo if serve ad evitare che le varie stringhe vengano sostituite da "\n" e sue varianti tipo "\n "
-                                break;
-                        switch (tagName){
-                            case "nome":
-                                nome = s;
-                                break;
-                            case "cognome":
-                                cognome = s;
-                                break;
-                            case "sesso":
-                                sesso = s;
-                                break;
-                            case "comune_nascita":
-                                comune = s;
-                                break;
-                            case "data_nascita":
-                                dataNascita = DataProcessing.toDate(s);
-                                break;
-                        }
-                        break;
-                }
-                xmlr.next();
-            }
-        } catch (Exception e) {
-            System.out.println("Errore in Input Persone");
-            System.out.println(e.getMessage());
-        }
-    }
+		try {
+			int id = 0;
+			String tagName = "";
+			String nome = "", cognome = "", comune = "", sesso = "";
+			Data dataNascita = null;
+			xmlif = XMLInputFactory.newInstance();
+			xmlr = xmlif.createXMLStreamReader(new FileInputStream("XML/input/" + filenamePers));
+			while (xmlr.hasNext()) { // continua a leggere finché ha eventi a disposizione
+				switch (xmlr.getEventType()) { // switch sul tipo di evento
+					case XMLStreamConstants.START_ELEMENT: // inizio di un elemento
+						tagName = xmlr.getLocalName();
+						if (tagName.equals("persona"))
+							id = Integer.parseInt(xmlr.getAttributeValue(0));
+						break;
+					case XMLStreamConstants.END_ELEMENT: // fine di un elemento
+						if (xmlr.getLocalName().equals("persona"))
+							DataProcessing.addToPersone(new Persona(nome, cognome, comune, sesso, dataNascita, id)); //quando arriva all'endtag costruisce la persona
+						break;
+					case XMLStreamConstants.CHARACTERS:
+						String s = xmlr.getText();
+						if (s.contains("\n")) //Questo if serve ad evitare che le varie stringhe vengano sostituite da "\n" e sue varianti tipo "\n "
+							break;
+						switch (tagName) {
+							case "nome":
+								nome = s;
+								break;
+							case "cognome":
+								cognome = s;
+								break;
+							case "sesso":
+								sesso = s;
+								break;
+							case "comune_nascita":
+								comune = s;
+								break;
+							case "data_nascita":
+								dataNascita = DataProcessing.toDate(s);
+								break;
+						}
+						break;
+				}
+				xmlr.next();
+			}
+		} catch (Exception e) {
+			System.out.println("Errore in Input Persone");
+			System.out.println(e.getMessage());
+		}
+	}
 
-    public static void readComuni()
-    {
-        XMLInputFactory xmlif = null;
-        XMLStreamReader xmlr = null;
+	/**
+	 * legge il file comuni e crea una nuova istanza di comuni e la adda alla lista comuni
+	 */
+	public static void readComuni() {
+		XMLInputFactory xmlif = null;
+		XMLStreamReader xmlr = null;
 
 		try {
 			xmlif = XMLInputFactory.newInstance();
@@ -123,4 +128,11 @@ public class Reader {
 		}
 	}
 
+	public static void setFilenameCod(String name) {
+		Reader.filenameCod = name;
+	}
+
+	public static void setFilenamePers(String name) {
+		Reader.filenamePers = name;
+	}
 }
