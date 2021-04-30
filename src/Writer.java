1f9077ee627542c81f31a4578c60a1474d1ef8f9
Trugix <r.signoroni001@studenti.unibs.it>
2021-04-30 15:00:57 +0200

    sostituito spazi con \t

--- src/Writer.java
+++ src/Writer.java
@@ -15,76 +15,76 @@ public class Writer {
 			xmlw.writeStartDocument("utf-8", "1.0");
 			xmlw.writeCharacters("\n");
 			xmlw.writeStartElement("output"); // scrittura del tag radice <output>
-			xmlw.writeCharacters("\n");
+			xmlw.writeCharacters("\n\t");
 			xmlw.writeStartElement("persone");
 			xmlw.writeAttribute("number", Integer.toString(DataProcessing.getPersone().size()));
-			xmlw.writeCharacters("\n    ");
+			xmlw.writeCharacters("\n\t\t");
 			for (Persona p : DataProcessing.getPersone()) {
 				xmlw.writeStartElement("persona");
 				xmlw.writeAttribute("id", Integer.toString(p.getId()));
-				xmlw.writeCharacters("\n        ");
+				xmlw.writeCharacters("\n\t\t\t");
 				xmlw.writeStartElement("nome");
 				xmlw.writeCharacters(p.getNome());
 				xmlw.writeEndElement(); //</nome>
-				xmlw.writeCharacters("\n        ");
+				xmlw.writeCharacters("\n\t\t\t");
 				xmlw.writeStartElement("cognome");
 				xmlw.writeCharacters(p.getCognome());
 				xmlw.writeEndElement(); //</cognome>
-				xmlw.writeCharacters("\n        ");
+				xmlw.writeCharacters("\n\t\t\t");
 				xmlw.writeStartElement("sesso");
 				xmlw.writeCharacters(p.getSesso());
 				xmlw.writeEndElement();//</sesso>
-				xmlw.writeCharacters("\n        ");
+				xmlw.writeCharacters("\n\t\t\t");
 				xmlw.writeStartElement("comune_nascita");
 				xmlw.writeCharacters(p.getLuogoNascita());
 				xmlw.writeEndElement();//</luogo_nascita>
-				xmlw.writeCharacters("\n        ");
+				xmlw.writeCharacters("\n\t\t\t");
 				xmlw.writeStartElement("data_nascita");
 				xmlw.writeCharacters(p.getData().dataToString());
 				xmlw.writeEndElement();//</data_nascita>
-				xmlw.writeCharacters("\n        ");
+				xmlw.writeCharacters("\n\t\t\t");
 				xmlw.writeStartElement("codice_fiscale");
 				if (p.isMatched()) xmlw.writeCharacters(p.getCodiceFiscale());
 				else xmlw.writeCharacters("ASSENTE");
 				xmlw.writeEndElement(); //</codice_fiscale>
-				xmlw.writeCharacters("\n    ");
+				xmlw.writeCharacters("\n\t\t");
 				xmlw.writeEndElement(); //</persona>
-				xmlw.writeCharacters("\n    ");
+				xmlw.writeCharacters("\n\t\t");
 			}
 			xmlw.writeEndElement(); // </persone>
-			xmlw.writeCharacters("\n");
+			xmlw.writeCharacters("\n\t");
 			xmlw.writeStartElement("codici");
 			xmlw.writeAttribute("numero", Integer.toString(DataProcessing.getCodici().size()));
-			xmlw.writeCharacters("\n    ");
+			xmlw.writeCharacters("\n\t\t");
 
 			xmlw.writeStartElement("invalidi");
 			xmlw.writeAttribute("numero", Integer.toString(DataProcessing.getCodiciInvalidi().size()));
-			xmlw.writeCharacters("\n        ");
+			xmlw.writeCharacters("\n\t\t\t");
 			int i = 0;
 			for (String s : DataProcessing.getCodiciInvalidi()) {
 				xmlw.writeStartElement("codice");
 				xmlw.writeAttribute("id", Integer.toString(i));
 				xmlw.writeCharacters(s);
 				xmlw.writeEndElement(); //</codice>
-				xmlw.writeCharacters("\n        ");
+				xmlw.writeCharacters("\n\t\t\t");
 				i++;
 			}
 			xmlw.writeEndElement(); //</invalidi>
-			xmlw.writeCharacters("\n    ");
+			xmlw.writeCharacters("\n\t\t");
 			xmlw.writeStartElement("spaiati");
 			xmlw.writeAttribute("numero", Integer.toString(DataProcessing.getCodiciSpaiati().size()));
-			xmlw.writeCharacters("\n        ");
+			xmlw.writeCharacters("\n\t\t\t");
 			i = 0;
 			for (String s : DataProcessing.getCodiciSpaiati()) {
 				xmlw.writeStartElement("codice");
 				xmlw.writeAttribute("id", Integer.toString(i));
 				xmlw.writeCharacters(s);
 				xmlw.writeEndElement();//</codice>
-				xmlw.writeCharacters("\n        ");
+				xmlw.writeCharacters("\n\t\t\t");
 				i++;
 			}
 			xmlw.writeEndElement();//</spaiati>
-			xmlw.writeCharacters("\n    ");
+			xmlw.writeCharacters("\n\t");
 			xmlw.writeEndElement();//</codici>
 			xmlw.writeCharacters("\n");
 			xmlw.writeEndElement();//</output>
